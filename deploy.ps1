083ce3bf9cbd96bea51af5baf6a75fadb8c227f3
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2021-01-16 11:52:19 -0300

    Removed manual deply files

--- deploy.ps1
+++
@@ -1,2 +0,0 @@
-$PWD=(Get-Item .\).FullName
-docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material gh-deploy
--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material gh-deploy
